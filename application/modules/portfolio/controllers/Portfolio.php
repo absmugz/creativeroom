3b5073bb55cb777bbc72ab3ceba46e1eddc36cbe
absmugz <absmugz09@gmail.com>
2016-01-16 21:26:38 +0200

    setting up hmvc and restapi

--- application/modules/portfolio/controllers/Portfolio.php
+++ application/modules/portfolio/controllers/Portfolio.php
@@ -1,26 +1,52 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
+require(APPPATH.'modules\portfolio\libraries\REST_Controller.php');
 
+class Portfolio extends Rest_Controller {
+    
+public function __construct() {
+        parent::__construct();
+$this->load->model('actor_model');
 
-class Portfolio extends MX_Controller {
-
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see https://codeigniter.com/user_guide/general/urls.html
-	 */
+    }
 	public function index()
 	{
 		echo 'This is HMVC at work';
+                
+                $result = $this->customers_model->get_all();
+                var_dump($result);die();
 	}
-}
+        
+      
+function portfolios_get()
+    {
+        // respond with information about several portfolios
+    //$portfolios = array (
+    //    1=> array('title'=>'dhafie','description'=>'this is dhafie'),
+    //    2=> array('title'=>'Aly','description'=>'this is Princess Aly')
+    //);
+    
+     $portfolios = $this->actor_model->get_all();
+    
+    $this->response($portfolios);
+    
+    }
+ 
+        
+ function portfolio_item_get()
+    {
+        // respond with information about a item
+     $id = $this->uri->segment(3);
+     //$portfolios = array (
+    //    1=> array('title'=>'dhafie','description'=>'this is dhafie'),
+    //    2=> array('title'=>'Aly','description'=>'this is Princess Aly')
+    //);
+    
+     $portfolio = $this->actor_model->get($id);
+    
+    $this->response($portfolio);
+    }
+
+
+
+ }
---
+++ application/modules/portfolio/models/actor_model.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of customers
+ *
+ * @author Dhafie
+ */
+
+
+class Actor_model extends MY_Model
+{
+public $_table = 'actor';
+public $primary_key = 'actor_id';
+}
